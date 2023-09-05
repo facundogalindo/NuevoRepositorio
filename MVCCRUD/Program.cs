e4682830de36301f04302c047090910c3ee11e47
Facundo Galindo <galindofacundo@gmail.com>
2023-09-05 19:30:25 -0300

    Crud Basico (Crear, consultar, modificara y eliminar) con botones

--- MVCCRUD/Program.cs
+++ MVCCRUD/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MVCCRUD.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<MvccrudContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("conexion")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
